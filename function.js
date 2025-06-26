9d9324affe46b42556345e6c6c0ce4c9a36249e0
Yateesh Gangwar <152433915+Yateesh8@users.noreply.github.com>
2025-06-27 00:29:55 +0530

    Update functions.js

--- function.js
+++ function.js
@@ -1,5 +1,24 @@
-function num (a){
-    return a+1;
+function greet(name) {
+  return `Hello, ${name}!`;
 }
-console.log(num(1));
-num(2); 
+
+console.log(greet("Yateesh"));  // Output: Hello, Yateesh!
+
+# First class function in JavaScript ( You can assign functions to variables, can pass functions as arguments, can return functions from other functions.)
+
+const describeGoal = function() { // A function that describes your goal
+  return "I want to master DSA and Web Development!";
+};
+
+function showJourney(fn) { // A function that accepts another function as input
+  console.log("Your journey so far:");
+  console.log(fn());
+}
+
+showJourney(describeGoal);
+
+//Output: Your journey so far:
+// I want to master DSA and Web Development!
+
+
+    
